04eb61f9ce92967167da2b503b69caf6c92a907d
tomerun <tomerunmail@gmail.com>
2016-08-05 15:36:13 +0900

    use Rational

--- filling_solver/src/FillingSolver.java
+++ filling_solver/src/FillingSolver.java
@@ -6,55 +6,25 @@ import java.util.Scanner;
 
 public class FillingSolver {
 
-	ArrayList<Point<BigInteger>> points = new ArrayList<>();
+	ArrayList<Point<Rational>> points = new ArrayList<>();
 	int[][] polygons;
 	Edge[] edges;
-	BigInteger scale;
 
 	void readInput() {
 		try (Scanner sc = new Scanner(System.in)) {
+			HashMap<Point<Rational>, Integer> pointToIdx = new HashMap<>();
 			int NP = sc.nextInt();
-			ArrayList<ArrayList<Point<Rational>>> rawPolygons = new ArrayList<>();
-			scale = BigInteger.ONE;
+			polygons = new int[NP][];
 			for (int i = 0; i < NP; ++i) {
 				int NV = sc.nextInt();
+				polygons[i] = new int[NV];
 				ArrayList<Point<Rational>> polygon = new ArrayList<>();
 				for (int j = 0; j < NV; ++j) {
 					String[] coord = sc.next().split(",");
 					Rational x = new Rational(coord[0]);
 					Rational y = new Rational(coord[1]);
-					polygon.add(new Point<Rational>(x, y));
-					scale = lcm(scale, x.den);
-					scale = lcm(scale, y.den);
-				}
-				rawPolygons.add(polygon);
-			}
-			int NE = sc.nextInt();
-			ArrayList<ArrayList<Point<Rational>>> rawEdges = new ArrayList<>();
-			for (int i = 0; i < NE; ++i) {
-				ArrayList<Point<Rational>> edge = new ArrayList<>();
-				String[] pos1 = sc.next().split(",");
-				Rational x1 = new Rational(pos1[0]);
-				Rational y1 = new Rational(pos1[1]);
-				edge.add(new Point<Rational>(x1, y1));
-				String[] pos2 = sc.next().split(",");
-				Rational x2 = new Rational(pos2[0]);
-				Rational y2 = new Rational(pos2[1]);
-				edge.add(new Point<Rational>(x2, y2));
-				scale = lcm(scale, x1.den);
-				scale = lcm(scale, y1.den);
-				scale = lcm(scale, x2.den);
-				scale = lcm(scale, y2.den);
-				rawEdges.add(edge);
-			}
-
-			HashMap<Point<BigInteger>, Integer> pointToIdx = new HashMap<>();
-			polygons = new int[NP][];
-			for (int i = 0; i < NP; ++i) {
-				ArrayList<Point<Rational>> polygon = rawPolygons.get(i);
-				polygons[i] = new int[polygon.size()];
-				for (int j = 0; j < polygon.size(); ++j) {
-					Point<BigInteger> p = scaleToInt(polygon.get(j), scale);
+					Point<Rational> p = new Point<Rational>(x, y);
+					polygon.add(p);
 					if (!pointToIdx.containsKey(p)) {
 						pointToIdx.put(p, points.size());
 						points.add(p);
@@ -62,26 +32,29 @@ public class FillingSolver {
 					polygons[i][j] = pointToIdx.get(p);
 				}
 			}
+			int NE = sc.nextInt();
 			edges = new Edge[NE];
 			for (int i = 0; i < NE; ++i) {
-				int p1, p2;
-				{
-					Point<BigInteger> p = scaleToInt(rawEdges.get(i).get(0), scale);
-					if (!pointToIdx.containsKey(p)) {
-						pointToIdx.put(p, points.size());
-						points.add(p);
-					}
-					p1 = pointToIdx.get(p);
+				String[] pos1 = sc.next().split(",");
+				Rational x1 = new Rational(pos1[0]);
+				Rational y1 = new Rational(pos1[1]);
+				Point<Rational> p1 = new Point<Rational>(x1, y1);
+				if (!pointToIdx.containsKey(p1)) {
+					pointToIdx.put(p1, points.size());
+					points.add(p1);
 				}
-				{
-					Point<BigInteger> p = scaleToInt(rawEdges.get(i).get(1), scale);
-					if (!pointToIdx.containsKey(p)) {
-						pointToIdx.put(p, points.size());
-						points.add(p);
-					}
-					p2 = pointToIdx.get(p);
+				int ep1 = pointToIdx.get(p1);
+
+				String[] pos2 = sc.next().split(",");
+				Rational x2 = new Rational(pos2[0]);
+				Rational y2 = new Rational(pos2[1]);
+				Point<Rational> p2 = new Point<Rational>(x2, y2);
+				if (!pointToIdx.containsKey(p2)) {
+					pointToIdx.put(p2, points.size());
+					points.add(p2);
 				}
-				edges[i] = new Edge(p1, p2);
+				int ep2 = pointToIdx.get(p2);
+				edges[i] = new Edge(ep1, ep2);
 			}
 		}
 	}
